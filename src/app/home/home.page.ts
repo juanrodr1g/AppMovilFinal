1414e93e4292e2863a90a72a72b7cf2d1288a644
Juan Rodríguez Quijada <45419841+juanrodr1g@users.noreply.github.com>
2020-02-18 00:08:41 +0100

    Subida proyecto

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,192 @@
+import { Component, OnInit } from '@angular/core';
+import { ImagePicker } from '@ionic-native/image-picker/ngx';
+import { ActionSheetController, Platform } from '@ionic/angular';
+import {
+  MediaCapture,
+  MediaFile,
+  CaptureError
+} from '@ionic-native/media-capture/ngx';
+import { File, FileEntry } from '@ionic-native/File/ngx';
+import { Media, MediaObject } from '@ionic-native/media/ngx';
+import { StreamingMedia } from '@ionic-native/streaming-media/ngx';
+import { PhotoViewer } from '@ionic-native/photo-viewer/ngx';
+ 
+const MEDIA_FOLDER_NAME = 'app_archivos';
+ 
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss']
+})
+export class HomePage implements OnInit {
+  files = [];
+ 
+  constructor(
+    private imagePicker: ImagePicker,
+    private mediaCapture: MediaCapture,
+    private file: File,
+    private media: Media,
+    private streamingMedia: StreamingMedia,
+    private photoViewer: PhotoViewer,
+    private actionSheetController: ActionSheetController,
+    private plt: Platform
+  ) {}
+ 
+  ngOnInit() {
+    this.plt.ready().then(() => {
+      let path = this.file.dataDirectory;
+      this.file.checkDir(path, MEDIA_FOLDER_NAME).then(
+        () => {
+          this.loadFiles();
+        },
+        err => {
+          this.file.createDir(path, MEDIA_FOLDER_NAME, false);
+        }
+      );
+    });
+  }
+ 
+  loadFiles() {
+    this.file.listDir(this.file.dataDirectory, MEDIA_FOLDER_NAME).then(
+      res => {
+        this.files = res;
+      },
+      err => console.log('error loading files: ', err)
+    );
+  }
+async selectMedia() {
+    const actionSheet = await this.actionSheetController.create({
+      header: '---- Menu de Acciones --- ',
+      buttons: [
+        {
+          text: 'Capturar Foto',
+          handler: () => {
+            this.captureImage();
+          }
+        },
+        {
+          text: 'Grabar Video',
+          handler: () => {
+            this.recordVideo();
+          }
+        },
+        {
+          text: 'Grabar un audio',
+          handler: () => {
+            this.recordAudio();
+          }
+        },
+        {
+          text: 'Cargar multiples fotos',
+          handler: () => {
+            this.pickImages();
+          }
+        },
+        {
+          text: 'Cancelar',
+          role: 'cancel'
+        }
+      ]
+    });
+    await actionSheet.present();
+  }
+ 
+  pickImages() {
+    this.imagePicker.getPictures({}).then(
+      results => {
+        for (var i = 0; i < results.length; i++) {
+          this.copyFileToLocalDir(results[i]);
+        }
+      }
+    );
+ 
+    // If you get problems on Android, try to ask for Permission first
+    // this.imagePicker.requestReadPermission().then(result => {
+    //   console.log('requestReadPermission: ', result);
+    //   this.selectMultiple();
+    // });
+  }
+ 
+  captureImage() {
+    this.mediaCapture.captureImage().then(
+      (data: MediaFile[]) => {
+        if (data.length > 0) {
+          this.copyFileToLocalDir(data[0].fullPath);
+        }
+      },
+      (err: CaptureError) => console.error(err)
+    );
+  }
+ 
+  recordAudio() {
+    this.mediaCapture.captureAudio().then(
+      (data: MediaFile[]) => {
+        if (data.length > 0) {
+          this.copyFileToLocalDir(data[0].fullPath);
+        }
+      },
+      (err: CaptureError) => console.error(err)
+    );
+  }
+ 
+  recordVideo() {
+    this.mediaCapture.captureVideo().then(
+      (data: MediaFile[]) => {
+        if (data.length > 0) {
+          this.copyFileToLocalDir(data[0].fullPath);
+        }
+      },
+      (err: CaptureError) => console.error(err)
+    );
+  }
+  copyFileToLocalDir(fullPath) {
+    let myPath = fullPath;
+    // Make sure we copy from the right location
+    if (fullPath.indexOf('file://') < 0) {
+      myPath = 'file://' + fullPath;
+    }
+ 
+    const ext = myPath.split('.').pop();
+    const d = Date.now();
+    var newName = prompt("¿Que nombre le quieres poner?") ;
+    if(newName!=null){
+newName=`${newName}.${ext}`;
+    }else{
+      newName= `${d}.${ext}`
+    }
+    const name = myPath.substr(myPath.lastIndexOf('/') + 1);
+    const copyFrom = myPath.substr(0, myPath.lastIndexOf('/') + 1);
+    const copyTo = this.file.dataDirectory + MEDIA_FOLDER_NAME;
+ 
+    this.file.copyFile(copyFrom, name, copyTo, newName).then(
+      success => {
+        this.loadFiles();
+      },
+      error => {
+        console.log('error: ', error);
+      }
+    );
+  }
+ 
+  openFile(f: FileEntry) {
+    if (f.name.indexOf('.wav') > -1) {
+      // We need to remove file:/// from the path for the audio plugin to work
+      const path =  f.nativeURL.replace(/^file:\/\//, '');
+      const audioFile: MediaObject = this.media.create(path);
+      audioFile.play();
+    } else if (f.name.indexOf('.MOV') > -1 || f.name.indexOf('.mp4') > -1 || f.name.indexOf('.amr') > -1|| f.name.indexOf('.mkv') > -1 ) {
+      // E.g: Use the Streaming Media plugin to play a video
+      this.streamingMedia.playVideo(f.nativeURL);
+    } else if (f.name.indexOf('.jpg') > -1) {
+      // E.g: Use the Photoviewer to present an Image
+      this.photoViewer.show(f.nativeURL, 'Mi imagen: ');
+    }
+  }
+ 
+  deleteFile(f: FileEntry) {
+    const path = f.nativeURL.substr(0, f.nativeURL.lastIndexOf('/') + 1);
+    this.file.removeFile(path, f.name).then(() => {
+      this.loadFiles();
+    }, err => console.log('error remove: ', err));
+  }
+}
